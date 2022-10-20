8da5958645831614e024450a5b4e87cbbbbce7e4
karthick kannan <kannankund321@gmail.com>
2022-10-20 13:53:16 +0530

    first commit

---
+++ Seat booking/script/script.js
@@ -0,0 +1,74 @@
+const container = document.querySelector('.screen-container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+
+const movieSelect = document.getElementById('movie');
+
+populateUI();
+
+// const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
+//save selected movie Index and Price
+const setMovieData = function (movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
+//functions
+const updateSelectedCount = function () {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  //copy selected seats into array
+  //map through array
+  //return a new array
+  const seatsIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = ticketPrice * selectedSeatsCount;
+};
+
+//event listeners
+
+//Get data from LocalStorage and Populate UI
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+//movie select
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+//seat select
+container.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
+
+//initial count and total setMovieData
+updateSelectedCount();
